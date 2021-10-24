d2785ad621e4db52efb083357eb65fcbaf61cab1
Gerardo Schekaiban <ggschekaiban@hotmail.com>
2021-10-23 21:37:27 -0500

    Add gallery section

--- js/app.js
+++ js/app.js
@@ -7,4 +7,20 @@ const navSlide = () => {
     });
 }
 
+function createGallery(){
+    const gallery = document.querySelector('.gallery-images');
+
+    for(let i = 1; i<=12; i++){
+        const image = document.createElement('picture');
+        image.innerHTML = `
+        <source srcset="./img/galeria${i}.jpg" type="image/jpg">
+        <source srcset="./img/galeria${i}.jpg" type="image/jpg">
+        <img loading="lazy" width="200" height="300" src="./img/galeria${i}.jpg" alt="gallery image">
+    `;
+
+        gallery.appendChild(image);
+    }
+}
+
 navSlide();
+createGallery();
